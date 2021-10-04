18af6f1de561e4c46f66f9b09347e39107d2e456
Junhwan Kim <junhwan.tech@gmail.com>
2021-10-04 17:25:24 +0900

    test: Parameterized Test를 활용해서 Set에 값이 존재하는지 테스트 (study)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -34,5 +36,12 @@ public class SetTest {
         assertThat(numbers.contains(1)).isTrue();
         assertThat(numbers.contains(2)).isTrue();
         assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Parameterized Test로 Set 중복코드를 제거할 수 있다.")
+    void testContainsUsingParameterizedTest(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
