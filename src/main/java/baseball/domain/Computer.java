6a49e29fc524f80cd5105a7e8d79e80f0c8e7778
Junhwan Kim <junhwan.tech@gmail.com>
2021-10-06 00:37:51 +0900

    refactor: rename constant

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -6,49 +6,49 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Computer {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 9;
-    private static final int DIGIT_LENGTH = 3;
+	private static final int PICK_MINIMUM = 1;
+	private static final int PICK_MAXIMUM = 9;
+	private static final int DIGIT_LENGTH = 3;
 
-    private String numbers;
+	private String numbers;
 
-    public Computer() {
-    }
+	public Computer() {
+	}
 
-    public String getNumbers() {
-        if (this.numbers == null) {
-            setNumbers();
-        }
+	public String getNumbers() {
+		if (this.numbers == null) {
+			setNumbers();
+		}
 
-        return this.numbers;
-    }
+		return this.numbers;
+	}
 
-    private void setNumbers() {
-        Set<Character> digits = generateNonDuplicateDigits();
+	private void setNumbers() {
+		Set<Character> digits = generateNonDuplicateDigits();
 
-        this.numbers = characterSetToString(digits);
-    }
+		this.numbers = characterSetToString(digits);
+	}
 
-    private String characterSetToString(Set<Character> digits) {
-        Character[] chars = digits.toArray(new Character[0]);
-        StringBuilder sb = new StringBuilder();
+	private String characterSetToString(Set<Character> digits) {
+		Character[] chars = digits.toArray(new Character[0]);
+		StringBuilder sb = new StringBuilder();
 
-        for (char c : chars) {
-            sb.append(c);
-        }
+		for (char c : chars) {
+			sb.append(c);
+		}
 
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 
-    private Set<Character> generateNonDuplicateDigits() {
-        Set<Character> digits = new LinkedHashSet<>();
-        int radix = 10;
+	private Set<Character> generateNonDuplicateDigits() {
+		Set<Character> digits = new LinkedHashSet<>();
+		int radix = 10;
 
-        do {
-            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-            digits.add(Character.forDigit(randomNumber, radix));
-        } while (digits.size() < DIGIT_LENGTH);
+		do {
+			int randomNumber = Randoms.pickNumberInRange(PICK_MINIMUM, PICK_MAXIMUM);
+			digits.add(Character.forDigit(randomNumber, radix));
+		} while (digits.size() < DIGIT_LENGTH);
 
-        return digits;
-    }
+		return digits;
+	}
 }
