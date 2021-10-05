f03f6693a360e71de8bc31b853f2318846b65c1a
Junhwan Kim <junhwan.tech@gmail.com>
2021-10-06 00:05:33 +0900

    feat: 게임 종료 시 새로운 게임을 시작하거나 종료 할 수 있도록 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -17,13 +17,29 @@ public class GameController {
     }
 
     public void run() {
+        do {
+            playGame();
+        } while(getContinueSelect());
+    }
+
+    private boolean getContinueSelect() {
+        String userSelect = inputView.inputContinueOrEnd();
+
+        while (!userSelect.equals("1") && !userSelect.equals("2")) {
+            userSelect = inputView.inputContinueOrEnd();
+        }
+
+        return userSelect.equals("1");
+    }
+
+    private void playGame() {
         Computer computer = new Computer();
         Game game = new Game(computer);
 
-        while(!game.isFinished()) {
+        do {
             String userNumber = inputView.inputNumber();
             outputView.printMessage(game.checkInputNumber(userNumber));
-        }
+        } while (!game.isFinished());
 
         outputView.printMessage("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
     }
